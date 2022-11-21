863bd6dbe10dfe1dddc4dd1745a530df1a7fec06
François Martineau <ffrancoismmartineau@gmail.com>
2022-11-21 08:06:48 -0500

    functional tests

--- _functional/main.c
+++ _functional/main.c
@@ -5,8 +5,15 @@ void f()
     printf("f executed\n");
 }
 
+void g()
+{
+    printf("g executed\n");
+}
+
 void a(void (*some_func)(void))
 {
+    some_func();
+    some_func = g;
     some_func();
 }
 
